0a7a5b2e5b6e0489a9c3538e7f39d19f254cf80d
Sabrina Souza <sabrinasouza.dev@gmail.com>
2024-12-03 21:50:49 -0300

    refactor: extract sections from home page into separate components

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,6 +1,7 @@
 import { Footer } from "../../components/Footer/Footer";
 import { NavBar } from "../../components/NavBar/NavBar";
 import { Link } from "react-router-dom";
+import { BrowseRange } from "./components/BrowseRange";
 
 export const HomePage = () => {
   return (
@@ -28,39 +29,9 @@ export const HomePage = () => {
           </Link>
         </div>
       </section>
-      <section className="flex flex-col justify-center items-center text-center text-gray-333 ">
-        <div className="my-14">
-          <h2 className=" text-3xl font-bold my-2">Browse The Range</h2>
-          <p className="text-gray-666 text-xl">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          </p>
-        </div>
-        <div className="flex">
-          <div className="mx-5">
-            <img
-              src="../../../src/assets/images/productCategory/image-106.png"
-              alt="image-106"
-              className="img-rd-cover-center"
-            />
-            <h3 className="text-gray-smb-24">Dining</h3>
-          </div>
-          <div className="mx-5">
-            <img
-              src="../../../src/assets/images/productCategory/image-100.png"
-              alt="image-100"
-              className="img-rd-cover-center"
-            />
-            <h3 className="text-gray-smb-24">Living</h3>
-          </div>
-          <div className="mx-5">
-            <img
-              src="../../../src/assets/images/productCategory/image-101.png"
-              alt="image-101"
-              className="img-rd-cover-center"
-            />
-            <h3 className="text-gray-smb-24">Bedroom</h3>
-          </div>
-        </div>
+      <BrowseRange />
+      <section>
+        
       </section>
       <Footer />
     </div>
---
+++ src/pages/HomePage/components/BrowseRange.tsx
@@ -0,0 +1,39 @@
+export const BrowseRange = () => {
+  return (
+    
+    <section className="flex flex-col justify-center items-center text-center text-gray-333 ">
+    <div className="my-14">
+      <h2 className=" text-3xl font-bold my-2">Browse The Range</h2>
+      <p className="text-gray-666 text-xl">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      </p>
+    </div>
+    <div className="flex">
+      <div className="mx-5">
+        <img
+          src="../../../../src/assets/images/productCategory/image-106.png"
+          alt="image-106"
+          className="img-rd-cover-center"
+        />
+        <h3 className="text-gray-smb-24">Dining</h3>
+      </div>
+      <div className="mx-5">
+        <img
+          src="../../../../src/assets/images/productCategory/image-100.png"
+          alt="image-100"
+          className="img-rd-cover-center"
+        />
+        <h3 className="text-gray-smb-24">Living</h3>
+      </div>
+      <div className="mx-5">
+        <img
+          src="../../../../src/assets/images/productCategory/image-101.png"
+          alt="image-101"
+          className="img-rd-cover-center"
+        />
+        <h3 className="text-gray-smb-24">Bedroom</h3>
+      </div>
+    </div>
+  </section>
+  )
+}
