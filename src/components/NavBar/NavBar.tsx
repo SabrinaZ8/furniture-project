6d0644f4f9e4ef026fc369b00d906a6788d3337e
Sabrina Souza <sabrinasouza.dev@gmail.com>
2024-12-06 17:13:30 -0300

    feat: add login and logout in navBar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,29 +1,65 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import { TbUserExclamation } from "react-icons/tb";
 import { AiOutlineShoppingCart } from "react-icons/ai";
-
+import { UserButton, useClerk, SignedIn, SignedOut } from "@clerk/clerk-react";
 
 export const NavBar = () => {
+  const { redirectToSignIn } = useClerk();
+  const { signOut } = useClerk();
+
+  const handleLogin = () => {
+    redirectToSignIn();
+  };
+  const handleLogout = () => {
+    signOut();
+  };
+
   return (
     <nav className="flex justify-between items-center h-[100px] mx-14">
-      <Link to="/"  className="flex">
-        <img src="../../../public/LogoFurniro/furniroLogo.png" alt="Logo Furniro" className="mr-2"/>
+      <Link to="/" className="flex">
+        <img
+          src="/LogoFurniro/furniroLogo.png"
+          alt="Logo Furniro"
+          className="mr-2"
+        />
         <span className="font-montserrat font-bold text-3xl">Furniro</span>
       </Link>
       <div className="w-1/3">
         <ul className="flex font-medium justify-between">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/shop">Shop</Link></li>
-          <li><Link to="#">About</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/shop">Shop</Link>
+          </li>
+          <li>
+            <Link to="#">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
         </ul>
       </div>
       <div className="flex">
-        <TbUserExclamation className="w-7 h-6 mr-3.5" />
+        <div className=" mr-3.5 flex items-center justify-center">
+          <div onClick={handleLogout}>
+
+            {/* If the user is online, he can log out*/}
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </div>
+
+          {/*If the user is offline, he can click and log in*/}
+          <SignedOut>
+            <TbUserExclamation onClick={handleLogin} className="w-7 h-6" />
+          </SignedOut>
+          
+        </div>
         <Link to="/cart">
-          <AiOutlineShoppingCart className="w-7 h-6"/>
+          <AiOutlineShoppingCart className="w-7 h-6" />
         </Link>
       </div>
     </nav>
-  )
-}
+  );
+};
