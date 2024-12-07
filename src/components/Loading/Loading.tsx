d024fd280a7da0a0ee0789a8c3f60ff8ce1b1066
Sabrina Souza <sabrinasouza.dev@gmail.com>
2024-12-07 16:52:04 -0300

    feat: add loading spinner to project

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,20 @@
+import { BallTriangle } from "react-loader-spinner";
+
+export const Loading = () => {
+  return (
+    <div className="flex items-center flex-col justify-center h-screen">
+    <img src="/LogoFurniro/furniroLogo.png" alt="Logo" className="mb-3"/>
+    <span className="font-montserrat font-bold text-3xl mb-6">Furniro</span>
+      <BallTriangle
+        height={100}
+        width={100}
+        radius={5}
+        color="#B88E2F"
+        ariaLabel="ball-triangle-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+      />
+    </div>
+  );
+};
