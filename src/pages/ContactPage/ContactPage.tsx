6532e7ecb3ccb5da230b090ef4dbcf159537f85e
Sabrina Souza <sabrinasouza.dev@gmail.com>
2024-12-07 16:06:52 -0300

    feat: add contact page

--- src/pages/ContactPage/ContactPage.tsx
+++ src/pages/ContactPage/ContactPage.tsx
@@ -1,5 +1,84 @@
+import { Banner } from "../../components/Banner/Banner";
+import { Footer } from "../../components/Footer/Footer";
+import { MoreInfos } from "../../components/MoreInfos/MoreInfos";
+import { NavBar } from "../../components/NavBar/NavBar";
+import { FaMapMarkerAlt } from "react-icons/fa";
+import { FaPhoneAlt } from "react-icons/fa";
+import { MdAccessTimeFilled } from "react-icons/md";
+
 export const ContactPage = () => {
   return (
-    <div>ContactPage</div>
-  )
-}
+    <div>
+      <NavBar />
+      <Banner path={"Contact"} />
+
+      <section className="mt-[90px] mb-16">
+        <div className="flex flex-col text-center items-center">
+          <h2 className="text-4xl font-semibold">Get In Touch With Us</h2>
+          <p className="text-gray-350 w-4/12 mt-5">
+            For More Information About Our Product & Services. Please Feel Free
+            To Drop Us An Email. Our Staff Always Be There To Help You Out. Do
+            Not Hesitate!
+          </p>
+        </div>
+        <div className="flex mx-[237px]">
+          <div className="w-[30%] mt-9">
+            <div className="infos-address-phone">
+              <div className="mr-6">
+                <FaMapMarkerAlt className="infos-address-phone-icons" />
+              </div>
+              <div>
+                <h3 className="infos-address-phone-h3">Address</h3>
+                <p>236 5th SE Avenue, New York NY10000, United States</p>
+              </div>
+            </div>
+            <div>
+              <div className="infos-address-phone">
+                <div className="mr-6">
+                  <FaPhoneAlt className="infos-address-phone-icons" />
+                </div>
+                <div>
+                  <h3 className="infos-address-phone-h3">Phone</h3>
+                  <p>
+                    Mobile: +(84) 546-6789 <br />
+                    Hotline: +(84) 456-6789
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div  className="infos-address-phone">
+              <div className="mr-6">
+                <MdAccessTimeFilled className="infos-address-phone-icons" />
+              </div>
+              <div>
+                <h3 className="infos-address-phone-h3">Working Time</h3>
+                <p>Monday-Friday: 9:00 - 22:00 Saturday-Sunday: 9:00 - 21:00</p>
+              </div>
+            </div>
+          </div>
+          <div className="w-[70%] flex flex-col items-center">
+            <div className="infos-address-phone-input-div">
+              <label htmlFor="name" className="infos-address-phone-label">Your Name</label>
+              <input id="name" type="text" className="infos-address-phone-input" placeholder="Ex: Sabrina"/>
+            </div>
+            <div className="infos-address-phone-input-div">
+              <label htmlFor="email" className="infos-address-phone-label">Email address</label>
+              <input type="email" id="email"  className="infos-address-phone-input" placeholder="ex: Your_email@gmail.com"/>
+            </div>
+            <div className="infos-address-phone-input-div">
+              <label htmlFor="subject" className="infos-address-phone-label">Subject</label>
+              <input type="text" id="subject"  className="infos-address-phone-input" placeholder="This is an optional"/>
+            </div>
+            <div className="infos-address-phone-input-div">
+              <label htmlFor="message" className="infos-address-phone-label">Message</label>
+              <input type="text" id="message"  className="infos-address-phone-input h-[120px]" placeholder="Hi! i’d like to ask about"/>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <MoreInfos />
+      <Footer />
+    </div>
+  );
+};
