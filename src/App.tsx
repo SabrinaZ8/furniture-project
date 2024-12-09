8e51b4ac1851541adf0116d9d939dde8a932e5c0
Sabrina Souza <sabrinasouza.dev@gmail.com>
2024-12-09 08:44:03 -0300

    feat: add toast for project

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import Routers from "./routers"
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 function App() {
 
   return (
     <div className="font-poppins">
+       <ToastContainer autoClose={3500}/>
       <Routers />
     </div>
   )
