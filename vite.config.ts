5c1ac2914d7ecedae643129cd2e36844be531154
Sabrina Souza <sabrinasouza.dev@gmail.com>
2024-12-10 22:46:44 -0300

    config: vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
+
+
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   server: {
-    host: '0.0.0.0', 
-    port: 5173,     
+    host: '0.0.0.0',
+    port: 5173,
   },
+  build: {
+    outDir: './dist',        // Pasta onde os arquivos de produção serão gerados
+    assetsDir: 'assets',     // Pasta dentro de dist para armazenar assets (imagens, etc.)
+    rollupOptions: {
+      output: {
+        // Personalize o nome e estrutura dos arquivos, se necessário
+      }
+    }
+  }
 });
