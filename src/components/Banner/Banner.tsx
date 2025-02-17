3df1eaf57e634db367b10d3a7bd4fcb24880efc6
Sabrina Souza <sabrinasouza.dev@gmail.com>
2025-02-17 10:53:38 -0300

    feat: add Links

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,10 +1,11 @@
 import { IoIosArrowForward } from "react-icons/io";
+import { Link } from "react-router-dom";
 interface Path {
-    path: string;
+  path: string;
 }
 export const Banner = ({ path }: Path) => {
   return (
-    <div className="h-[330px] relative flex justify-center items-center " >
+    <div className="h-[330px] relative flex justify-center items-center ">
       <img
         src="https://res.cloudinary.com/ducw84byb/image/upload/v1739625728/banner-top_ictml2.jpg"
         alt=""
@@ -17,8 +18,10 @@ export const Banner = ({ path }: Path) => {
         />
         <h2 className="text-5xl mt-5 mb-4">{path}</h2>
         <div className="flex items-center">
-          <p className="font-semibold">Home</p>
-          <IoIosArrowForward className="mx-2 w-5 h-5"/>
+          <Link to={"/"}>
+            <p className="font-semibold">Home</p>
+          </Link>
+          <IoIosArrowForward className="mx-2 w-5 h-5" />
           <p className="font-light">{path}</p>
         </div>
       </div>
