af6bd88f1bb036bf7e3ed1c19f579ab6873d2637
Sabrina Souza <sabrinasouza.dev@gmail.com>
2025-02-17 17:18:49 -0300

    feat: adding responsiveness in the footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -8,9 +8,9 @@ import { FormSubscribe } from "./components/FormSubscribe";
 
 export const Footer = () => {
   return (
-    <footer className="min-h-[505px] h-[505px] ">
+    <footer className="min-h-[505px]">
       <hr />
-      <div className="my-12 flex justify-between h-9/12 px-24">
+      <div className="my-12 justify-between h-9/12 px-4 flex flex-col md:px-24 lg:flex-row ">
         <div className="flex flex-col gap-y-14">
           <span className="font-poppins text-2xl font-bold">Furniro.</span>
           <address className="not-italic text-gray-350">
@@ -20,7 +20,7 @@ export const Footer = () => {
             <br />
             FL 33134 USA <br />
           </address>
-          <div className="flex">
+          <div className="flex justify-center lg:justify-start">
             <a
               href="https://pt-br.facebook.com/login/device-based/regular/login/"
               target="_blank"
@@ -51,9 +51,10 @@ export const Footer = () => {
             </a>
           </div>
         </div>
-        <div className="flex flex-col gap-y-12 font-medium">
+        <div className="flex justify-center my-10 lg:my-0 lg:justify-start">
+        <div className="flex flex-col items-center gap-y-12 font-medium mr-20 lg:mr-40 lg:my-0 lg:items-start">
           <h3 className="text-gray-350">Links</h3>
-          <ul className="flex flex-col gap-y-10">
+          <ul className="flex flex-col items-center gap-y-10 lg:items-start">
             <li >
               <Link to="/" className="hover-text">Home</Link>
             </li>
@@ -68,9 +69,9 @@ export const Footer = () => {
             </li>
           </ul>
         </div>
-        <div className="flex flex-col gap-y-12 font-medium">
+        <div className="flex flex-col items-center gap-y-12 font-medium lg:my-0 lg:items-start">
           <h3 className="text-gray-350">Help</h3>
-          <ul className="flex flex-col gap-y-10">
+          <ul className="flex flex-col items-center gap-y-10 lg:items-start">
             <li>
               <Link to='#' className="hover-text">Payment Options</Link>
             </li>
@@ -82,15 +83,17 @@ export const Footer = () => {
             </li>
           </ul>
         </div>
-        <div className="flex flex-col gap-y-12 font-medium">
+        </div>
+        
+        <div className="flex flex-col items-center lg:items-start gap-y-12 font-medium">
           <h3 className="text-gray-350">Newsletter</h3>
           
           <FormSubscribe />
         </div>
       </div>
-      <div className="px-24">
+      <div className="mx-10 lg:mx-24">
         <hr />
-        <p  className="my-9">2023 furniro. All rights reverved</p>
+        <p  className="my-9 ">2023 furniro. All rights reverved</p>
       </div>
     </footer>
   );
