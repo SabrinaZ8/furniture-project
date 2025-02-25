a9302828a55b8a10fff156495b0c17c56e4912bc
Sabrina Souza <sabrinasouza.dev@gmail.com>
2025-02-25 14:20:00 -0300

    feat: add menu mobile

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -4,9 +4,12 @@ import { AiOutlineShoppingCart } from "react-icons/ai";
 import { UserButton, useClerk, SignedIn, SignedOut } from "@clerk/clerk-react";
 import { useState } from "react";
 import { CartAside } from "./components/CartAside";
+import { IoMenu } from "react-icons/io5";
+import { IoMdClose } from "react-icons/io";
 
 export const NavBar = () => {
   const [cartSide, setCartSide] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
 
   const { redirectToSignIn } = useClerk();
   const { signOut } = useClerk();
@@ -19,32 +22,77 @@ export const NavBar = () => {
   };
 
   return (
-    <nav className="flex justify-between items-center h-[100px] p-14 relative">
+    <nav className="flex justify-between items-center min-h-[100px] px-4 md:px-14 relative">
       <Link to="/" className="flex">
         <img
           src="../../../src/assets/images/LogoFurniro/furniroLogo.png"
           alt="Logo Furniro"
-          className="mr-2"
+          className="max-sm:w-10 max-sm:h-7 mr-2"
         />
-        <span className="font-montserrat font-bold text-3xl">Furniro</span>
+        <span className="font-montserrat flex items-center font-bold text-xl sm:text-3xl">
+          Furniro
+        </span>
       </Link>
-      <div className="w-1/3">
+      <div className="max-md:hidden w-1/2 lg:w-1/3">
         <ul className="flex font-medium justify-between">
           <li>
-            <Link to="/" className="hover-text">Home</Link>
+            <Link to="/" className="hover-text">
+              Home
+            </Link>
           </li>
           <li>
-            <Link to="/shop" className="hover-text">Shop</Link>
+            <Link to="/shop" className="hover-text">
+              Shop
+            </Link>
           </li>
           <li>
-            <Link to="#" className="hover-text">About</Link>
+            <Link to="#" className="hover-text">
+              About
+            </Link>
           </li>
           <li>
-            <Link to="/contact" className="hover-text">Contact</Link>
+            <Link to="/contact" className="hover-text">
+              Contact
+            </Link>
           </li>
         </ul>
       </div>
-      <div className="flex">
+      {/*Menu mobile */}
+      <div className="flex flex-col mt-5 h-full md:hidden">
+        <button onClick={() => setIsOpen(!isOpen)} className="">
+          <IoMenu className="w-[30px] h-[30px]" />
+        </button>
+        {isOpen ? (
+          <ul className="flex items-center bg-yellow-40 absolute py-5 px-8 flex-col font-medium justify-evenly w-screen h-screen left-0 top-0 overflow-hidden">
+            <li>
+              <button onClick={() => setIsOpen(!isOpen)} className="">
+                <IoMdClose className="w-[30px] h-[30px]" />
+              </button>
+            </li>
+            <li>
+              <Link to="/" className="hover-text">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/shop" className="hover-text">
+                Shop
+              </Link>
+            </li>
+            <li>
+              <Link to="#" className="hover-text">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link to="/contact" className="hover-text">
+                Contact
+              </Link>
+            </li>
+          </ul>
+        ) : null}
+      </div>
+      <div className="flex items-center">
         <div className=" mr-3.5 flex items-center justify-center">
           <div onClick={handleLogout}>
             {/* If the user is online, he can log out*/}
@@ -55,7 +103,10 @@ export const NavBar = () => {
 
           {/*If the user is offline, he can click and log in*/}
           <SignedOut>
-            <TbUserExclamation onClick={handleLogin} className="w-7 h-6 hover:text-yellow-550 cursor-pointer" />
+            <TbUserExclamation
+              onClick={handleLogin}
+              className="w-7 h-6 hover:text-yellow-550 cursor-pointer"
+            />
           </SignedOut>
         </div>
 
