f1e0c44979981a6f8ab1786554be9300eb837a98
Sabrina Souza <sabrinasouza.dev@gmail.com>
2025-03-04 19:56:16 -0300

    feat: add translation support to contactPage

--- src/pages/ContactPage/ContactPage.tsx
+++ src/pages/ContactPage/ContactPage.tsx
@@ -9,6 +9,8 @@ import { MdAccessTimeFilled } from "react-icons/md";
 import { useValidateFormContact } from "../../hooks/useValidateFormContact";
 import { toast } from "react-toastify";
 import { FormContact } from "./components/FormContact";
+import { useTranslation } from "react-i18next"
+
 
 export const ContactPage = () => {
   const [formContact, setFormContact] = useState({
@@ -50,6 +52,8 @@ export const ContactPage = () => {
     }
   };
 
+  const { t } = useTranslation();
+
   return (
     <div>
       <NavBar />
@@ -57,11 +61,9 @@ export const ContactPage = () => {
 
       <section className="mt-[90px] mb-16">
         <div className="flex flex-col text-center items-center">
-          <h2 className="font-semibold text-3xl xl:text-4xl ">Get In Touch With Us</h2>
+          <h2 className="font-semibold text-3xl xl:text-4xl ">{t("getInTouch")}</h2>
           <p className="text-gray-350 w-5/6 lg:w-4/12 mt-5">
-            For More Information About Our Product & Services. Please Feel Free
-            To Drop Us An Email. Our Staff Always Be There To Help You Out. Do
-            Not Hesitate!
+            {t("contactText")}
           </p>
         </div>
         <div className="flex flex-col items-center xl:items-start mx-4 sm:mx-10 xl:mx-[237px] xl:flex-row">
@@ -71,7 +73,7 @@ export const ContactPage = () => {
                 <FaMapMarkerAlt className="infos-address-phone-icons" />
               </div>
               <div>
-                <h3 className="infos-address-phone-h3">Address</h3>
+                <h3 className="infos-address-phone-h3">{t("address")}</h3>
                 <p>236 5th SE Avenue, New York NY10000, United States</p>
               </div>
             </div>
@@ -81,7 +83,7 @@ export const ContactPage = () => {
                   <FaPhoneAlt className="infos-address-phone-icons" />
                 </div>
                 <div>
-                  <h3 className="infos-address-phone-h3">Phone</h3>
+                  <h3 className="infos-address-phone-h3">{t("phone")}</h3>
                   <p>
                     Mobile: +(84) 546-6789 <br />
                     Hotline: +(84) 456-6789
@@ -94,8 +96,8 @@ export const ContactPage = () => {
                 <MdAccessTimeFilled className="infos-address-phone-icons" />
               </div>
               <div>
-                <h3 className="infos-address-phone-h3">Working Time</h3>
-                <p>Monday-Friday: 9:00 - 22:00 Saturday-Sunday: 9:00 - 21:00</p>
+                <h3 className="infos-address-phone-h3">{t("workingTime")}</h3>
+                <p>{t("monday")}-{t("friday")}: 9:00 - 22:00 {t("saturday")}-{t("sunday")}: 9:00 - 21:00</p>
               </div>
             </div>
           </div>
--- src/pages/ContactPage/components/FormContact.tsx
+++ src/pages/ContactPage/components/FormContact.tsx
@@ -1,83 +1,82 @@
-import { FormContactComponent } from "../../../types/types"
+import { FormContactComponent } from "../../../types/types";
+import { useTranslation } from "react-i18next";
 
-export const FormContact:React.FC<FormContactComponent> = ({
-    handleSubmit,
-    formContact,
-    errors,
-    handleChange
+export const FormContact: React.FC<FormContactComponent> = ({
+  handleSubmit,
+  formContact,
+  errors,
+  handleChange,
 }) => {
-    return (
-        <form
-        className="flex flex-col items-center w-full sm:w-4/5 xl:w-1/2 xl:items-start"
-        onSubmit={(e) => handleSubmit(e)}
-      >
-        <div className="infos-address-phone-input-div">
-          <label htmlFor="name" className="infos-address-phone-label">
-            Your Name
-          </label>
-          <p className="message-error">{errors.name ? errors.name : ""}</p>
-          <input
-            id="name"
-            type="text"
-            className="infos-address-phone-input"
-            placeholder="Ex: Sabrina"
-            value={formContact.name}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="infos-address-phone-input-div">
-          <label htmlFor="email" className="infos-address-phone-label">
-            Email address
-          </label>
-          <p className="message-error">
-            {errors.email ? errors.email : ""}
-          </p>
-          <input
-            type="email"
-            id="email"
-            className="infos-address-phone-input"
-            placeholder="ex: Your_email@gmail.com"
-            value={formContact.email}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="infos-address-phone-input-div">
-          <label htmlFor="subject" className="infos-address-phone-label">
-            Subject
-          </label>
-          <input
-            type="text"
-            id="subject"
-            className="infos-address-phone-input"
-            placeholder="This is an optional"
-            value={formContact.subject}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="infos-address-phone-input-div">
-          <label htmlFor="message" className="infos-address-phone-label">
-            Message
-          </label>
-          <p className="message-error">
-            {errors.message ? errors.message : ""}
-          </p>
-          <input
-            type="text"
-            id="message"
-            className="infos-address-phone-input h-[120px]"
-            placeholder="Hi! i’d like to ask about"
-            value={formContact.message}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="my-[50px]">
-          <button
-            type="submit"
-            className="w-[237px] h-14 bg-yellow-550 rounded  text-white btn-transparent "
-          >
-            Submit
-          </button>
-        </div>
-      </form>
-    )
-}
+  const { t } = useTranslation();
+
+  return (
+    <form
+      className="flex flex-col items-center w-full sm:w-4/5 xl:w-1/2 xl:items-start"
+      onSubmit={(e) => handleSubmit(e)}
+    >
+      <div className="infos-address-phone-input-div">
+        <label htmlFor="name" className="infos-address-phone-label">
+          {t("yourName")}
+        </label>
+        <p className="message-error">{errors.name ? errors.name : ""}</p>
+        <input
+          id="name"
+          type="text"
+          className="infos-address-phone-input"
+          placeholder="Ex: Sabrina"
+          value={formContact.name}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="infos-address-phone-input-div">
+        <label htmlFor="email" className="infos-address-phone-label">
+          {t("enterEmail")}
+        </label>
+        <p className="message-error">{errors.email ? errors.email : ""}</p>
+        <input
+          type="email"
+          id="email"
+          className="infos-address-phone-input"
+          placeholder="ex: Your_email@gmail.com"
+          value={formContact.email}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="infos-address-phone-input-div">
+        <label htmlFor="subject" className="infos-address-phone-label">
+          {t("subject")}
+        </label>
+        <input
+          type="text"
+          id="subject"
+          className="infos-address-phone-input"
+          placeholder={t("thisIsOptional")}
+          value={formContact.subject}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="infos-address-phone-input-div">
+        <label htmlFor="message" className="infos-address-phone-label">
+          {t("message")}
+        </label>
+        <p className="message-error">{errors.message ? errors.message : ""}</p>
+        <input
+          type="text"
+          id="message"
+          className="infos-address-phone-input h-[120px]"
+          placeholder={t("hiIdLikeToAsk")}
+          value={formContact.message}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="my-[50px]">
+        <button
+          type="submit"
+          className="w-[237px] h-14 bg-yellow-550 rounded  text-white btn-transparent "
+        >
+          Submit
+        </button>
+      </div>
+    </form>
+  );
+};
